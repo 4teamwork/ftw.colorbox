0edb224ef9b01972a3c3ce8769eb19d0359f8a17
Julian Infanger <j.infanger@4teamwork.ch>
2011-12-05 08:17:07 +0100

    Addeed maxWidth and maxHeight. Images should not be bigger than 100%.

--- ftw/colorbox/browser/resources/colorbox-integration.js
+++ ftw/colorbox/browser/resources/colorbox-integration.js
@@ -1,6 +1,8 @@
 jq(function() {
     jq('a.colorboxLink').colorbox({
-        'photo': true,
-        'current': "{current}/{total}"
+      'photo': true,
+      'current': "{current}/{total}",
+      'maxWidth': '100%',
+      'maxHeight': '100%'
     });
 });
