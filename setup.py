238f63a599519174a93ce76279e6689a81742f0e
Thomas Buchberger <t.buchberger@4teamwork.ch>
2012-02-14 10:55:10 +0100

    Provide version number directly in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/colorbox/version.txt').read().strip()
+version = '1.0.2dev'
 maintainer = 'Julian Infanger'
 
 tests_require = [
