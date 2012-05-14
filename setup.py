a1f54b393a50a9d46a05118a278b87f6355a361e
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-05-14 17:37:10 +0200

    change package structure

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 setup(name='ftw.colorbox',
       version=version,
       description="An image gallery for Plone using ColorBox",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         "Framework :: Plone",
