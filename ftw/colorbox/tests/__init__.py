6982f03b16feb537dafc48250cc960dc5366a0ba
Martin Bächtold <github@tph.ch>
2015-08-12 14:51:49 +0200

    Add test setup.

---
+++ ftw/colorbox/tests/__init__.py
@@ -0,0 +1,17 @@
+from ftw.colorbox.testing import FTW_COLORBOX_FUNCTIONAL_TESTING
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from unittest2 import TestCase
+import transaction
+
+
+class FunctionalTestCase(TestCase):
+    layer = FTW_COLORBOX_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+    def grant(self, *roles):
+        setRoles(self.portal, TEST_USER_ID, roles)
+        transaction.commit()
--- setup.py
+++ setup.py
@@ -4,40 +4,46 @@ import os
 version = '1.2.2.dev0'
 
 tests_require = [
+    'ftw.testbrowser',
+    'ftw.testing',
     'plone.app.testing',
 ]
 
-setup(name='ftw.colorbox',
-      version=version,
-      description="An image gallery for Plone using ColorBox",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
+setup(
+    name='ftw.colorbox',
+    version=version,
+    description="An image gallery for Plone using ColorBox",
+    long_description='{0}\n{1}'.format(
+        open("README.rst").read(),
+        open(os.path.join("docs", "HISTORY.txt")).read()
+    ),
+
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
+    ],
 
-      keywords='ftw colorbox',
-      author='4teamwork AG',
-      author_email='mailto:info@4teamwork.ch',
-      url='https://github.com/4teamwork/ftw.colorbox',
-      license='GPL2',
+    keywords='ftw colorbox',
+    author='4teamwork AG',
+    author_email='mailto:info@4teamwork.ch',
+    url='https://github.com/4teamwork/ftw.colorbox',
+    license='GPL2',
 
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['ftw'],
-      include_package_data=True,
-      zip_safe=False,
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['ftw'],
+    include_package_data=True,
+    zip_safe=False,
 
-      install_requires=[
-          'setuptools',
-      ],
+    install_requires=[
+        'setuptools',
+    ],
 
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
 
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
